2047eeea2d522bfe8bfcf14e0d71a7d6ca12fbc7
Fay Wang <v-faywan@microsoft.com>
2020-06-23 16:32:41 +0800

    Add project files.

---
+++ WinrtApp/MainPage.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+#include "MainPage.g.cpp"
+#include <winrt/StaticLibrary1.h>
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::WinrtApp::implementation
+{
+    MainPage::MainPage()
+    {
+        InitializeComponent();
+    }
+
+    int32_t MainPage::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::ClickHandler(IInspectable const&, RoutedEventArgs const&)
+    {
+        myButton().Content(box_value(L"Clicked"));
+        StaticLibrary1::Class aa;
+        aa.showP1();
+    }
+}
